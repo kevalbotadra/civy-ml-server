5e77d809ee4f7fe510d8902d5fbb47f61e36e1e5
kevalbotadra <keval.botadra@gmail.com>
2021-07-29 23:38:14 -0400

    deploying to heroku

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import flask
+import gpt3
+import predict
+
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    budgetToPredict = flask.request.json["budgetList"]
+    monthToPredict = flask.request.json["monthToPredict"]
+    cateogry = flask.request.json["cateogry"]
+
+    budget = predict.predictBudget(budgetToPredict=budgetToPredict, monthToPredict=monthToPredict, cateogry=cateogry)
+
+    return {"budget" :  budget}
+
